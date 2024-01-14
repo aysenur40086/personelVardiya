052d815f389aa2653823d5817eb1e7804da942f0
Ayşenur Ödeyen <114692016+aysenur40086@users.noreply.github.com>
2024-01-14 12:13:37 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 import sys
 from cx_Freeze import setup, Executable
 
+# cx_Freeze için yapılandırma seçenekleri
 build_exe_options = {
     "packages": ["arayuz", "fonksiyonlar", "vt", "tkcalendar", "mysql.connector", "TKinterModernThemes", "numpy", "matplotlib"],
     "include_files": ["icon.ico"]
 }
 
+# Uygulama penceresi tipini belirleme
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+# Executable nesnesini oluşturma
 exe = Executable(
     script="main.py",
     base=base,
     icon="icon.ico"
 )
 
+# cx_Freeze setup fonksiyonu çağrısı
 setup(
     name="Personel Takip Sistemi",
     version="1.0",
