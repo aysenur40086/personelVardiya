77ea72310a18c1feecf35070a44073c4a36a9bdb
Ayşenur Ödeyen <114692016+aysenur40086@users.noreply.github.com>
2024-01-14 12:14:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+# arayuz modülünden App sınıfını içe aktar
 from arayuz import App
 
+# Ana fonksiyon
 def main():
+    # App sınıfından bir nesne oluştur
     app = App()
+    
+    # Uygulama döngüsünü başlat
     app.root.mainloop()
 
+# Eğer bu dosya doğrudan çalıştırılıyorsa main fonksiyonunu çağır
 if __name__ == "__main__":
     main()
